805a3a4f8f130ff31af651fc93c6a6ae1b210334
unknown <s.r.mahamid@gmail.com>
2024-09-19 23:37:20 -0700

    Cookie Value upgrade issue is still there leaderoard design in progress

--- navigation/screens/LeaderBoardScreen.tsx
+++ navigation/screens/LeaderBoardScreen.tsx
@@ -10,7 +10,12 @@ import {
 
 const LeaderBoardScreen = () => {
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+    <View
+      style={{
+        marginTop: hp(2),
+        alignItems: "center",
+      }}
+    >
       <View style={styles.headingBox}>
         <Text style={styles.heading}>LeaderBoard</Text>
       </View>
@@ -21,71 +26,11 @@ const LeaderBoardScreen = () => {
 export default LeaderBoardScreen;
 
 const styles = StyleSheet.create({
-  boxcontainer: {
-    borderWidth: wp(1.5),
-    borderColor: "#603928",
-    backgroundColor: "#f3b366",
-    height: wp(42),
-    width: wp(42),
-    alignItems: "center",
-    justifyContent: "center",
-    borderRadius: wp(8),
-  },
-  boxcontainer2: {
-    borderWidth: wp(1.5),
-    borderColor: "#603928",
-    backgroundColor: "#f3b366",
-    height: wp(15),
-    width: wp(42),
-    alignItems: "center",
-    justifyContent: "center",
-    borderRadius: wp(8),
-  },
-  boxtext: {
-    fontSize: wp(5),
-    color: "#603928",
-  },
-  itembox: {
-    borderWidth: wp(0.8),
-    borderColor: "#603928",
-    borderRadius: wp(2),
-    width: wp(85),
-    height: hp(7),
-
-    paddingHorizontal: wp(2),
-    flexDirection: "row",
-    justifyContent: "space-between",
-    backgroundColor: "#f3b366",
-  },
-  TEXT: {
-    color: "white",
-    fontSize: hp(2.2),
-    fontWeight: "bold",
-  },
-  Imagelook: {
-    width: wp(10),
-    height: hp(6),
-  },
-  Imagelook2: {
-    width: wp(12),
-    height: hp(8),
-  },
-  Imagelook3: {
-    width: wp(22),
-    height: hp(8),
-  },
-  Imagelook4: {
-    width: wp(10),
-    height: hp(8),
-    marginHorizontal: wp(5),
-  },
   heading: {
-    fontSize: hp(4),
+    fontSize: hp(4.5),
     fontWeight: "600",
     color: "#fabf5f",
-
     textAlign: "center",
-
     paddingTop: hp(0.5),
   },
   headingBox: {
@@ -94,8 +39,10 @@ const styles = StyleSheet.create({
     borderRadius: wp(5),
     borderColor: "#603928",
     backgroundColor: "#d27d2f",
-    height: hp(7.5),
+    height: hp(9.5),
+    width: wp(70),
     flexDirection: "row",
     justifyContent: "center",
+    alignItems: "center",
   },
 });
