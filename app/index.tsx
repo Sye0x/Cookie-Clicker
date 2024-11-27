bbae30c9161d81826decfd0a593b5709b5d20970
unknown <s.r.mahamid@gmail.com>
2024-11-27 21:23:42 +0500

    Issue added

--- app/index.tsx
+++ app/index.tsx
@@ -1,3 +1,5 @@
+//Game logic need tweaking so does scoreboard will try to complete this as soon as possible
+
 import MainContainer from "@/navigation/MainContainer";
 import { Text, View } from "react-native";
 
